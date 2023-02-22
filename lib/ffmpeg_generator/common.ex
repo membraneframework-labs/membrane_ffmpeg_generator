67953636db697bb098a026266a87792bcc2a8df7
Wojciech Barczynski <barczynski.w@gmail.com>
2023-02-22 18:01:49 +0100

    Remove common module doc, to erase module from docs

--- lib/ffmpeg_generator/common.ex
+++ lib/ffmpeg_generator/common.ex
@@ -1,7 +1,7 @@
 defmodule Membrane.FFmpegGenerator.Common do
-  @moduledoc """
-  Module providing functions used in both, video and audio generation.
-  """
+  @moduledoc false
+
+  # Module providing functions used in both, video and audio generation.
 
   @spec is_dir?(String.t()) :: boolean
   def is_dir?(path) do
